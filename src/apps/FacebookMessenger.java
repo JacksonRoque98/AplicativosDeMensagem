684f896c160afb26aad8321b3d98d7a4b7740f7f
Jackson Roque Pereira <jackson.roque98@icloud.com>
2023-08-27 16:21:43 -0300

    Sistema envio de mensagens, utilizando POO.

--- src/apps/FacebookMessenger.java
+++ src/apps/FacebookMessenger.java
@@ -1,12 +1,13 @@
 package apps;
 
 public class FacebookMessenger extends ServicoMensagemInstantanea {
+//@Override significa sobre-escrever o metodo que vem da classe ServicoMensagemInstantanea
     @Override
     public void enviarMensagem() {
         verificarConexaoInternet();
         System.out.println("Enviando mensagem pelo Facebook");
     }
-
+//@Override significa sobre-escrever o metodo que vem da classe ServicoMensagemInstantanea
     @Override
     public void receberMensagem() {
         System.out.println("Recebendo mensagem pelo Facebook");
--- src/apps/MSNMessenger.java
+++ src/apps/MSNMessenger.java
@@ -1,12 +1,13 @@
 package apps;
 
 public class MSNMessenger extends ServicoMensagemInstantanea{
+//@Override significa sobre-escrever o metodo que vem da classe ServicoMensagemInstantanea
     @Override
     public void enviarMensagem() {
         verificarConexaoInternet();
         System.out.println("Enviando mensagem pelo MSN");
     }
-
+//@Override significa sobre-escrever o metodo que vem da classe ServicoMensagemInstantanea
     @Override
     public void receberMensagem() {
         System.out.println("Recebendo mensagem pelo MSN");
